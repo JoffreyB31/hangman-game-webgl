6c7157787fa2d53f141742e0767a3fef1dc041c3
Joffrey <scyl6x@gmail.com>
2021-05-22 21:52:37 +0200

    add css keyboard + word display

---
+++ src/components/WordDisplay.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="word-display-container">
+    <div
+      class="word-display-letter"
+      v-for="(letter, index) in wordArray"
+      :key="'word-display-' + index"
+    >
+      {{ letter.toUpperCase() }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WordDisplayComponent",
+
+  props: {
+    word: { type: String, required: true },
+  },
+
+  computed: {
+    wordArray() {
+      return [...this.word];
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.word-display-container {
+  display: flex;
+  flex-wrap: nowrap;
+  margin: 0 auto;
+  justify-content: center;
+  margin: 5em 0;
+
+  .word-display-letter {
+    font-size: $wordDisplayFontSize;
+    width: 2%;
+
+    padding: $keyboardFontSize/2;
+    border: 5px solid black;
+    margin: 5px;
+    border-radius: 10px;
+    color: black;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/views/Play.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="play">
+    <WordDisplay :word="word" />
+    <Keyboard />
+  </div>
+</template>
+
+<script>
+import wordsJson from "@/assets/words.json";
+import WordDisplay from "@/components/WordDisplay.vue";
+import Keyboard from "@/components/Keyboard.vue";
+export default {
+  name: "PlayView",
+  components: {
+    Keyboard,
+    WordDisplay,
+  },
+  data: () => ({
+    word: undefined,
+  }),
+  mounted() {
+    this.word = this.getRandomWord();
+  },
+  methods: {
+    getRandomWord() {
+      const wordsArray = wordsJson.values;
+      return wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    },
+  },
+};
+</script>
+
+<style></style>
